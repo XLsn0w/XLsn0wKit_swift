75784f1da52a5f006731e57dbc1fe8130a344507
XLsn0w <xlsn0w@outlook.com>
2017-06-08 15:53:18 +0800

    pod trunk 1.0.5

--- XLsn0wKit/XLsn0wNetwork/XLsn0wNetworking.swift
+++ XLsn0wKit/XLsn0wNetwork/XLsn0wNetworking.swift
@@ -8,26 +8,22 @@
  *   /_/    \_\  |________| |________|   |_|    \__|  |_________|       \_/       \_/        *
  *                                                                                           *
  *********************************************************************************************/
-
-// tips：因为Swift存在namespace，所以就不需要写类前缀了
 import UIKit
 import Alamofire
 
-// 网络请求超时时间
-let NetworkTimeoutInterval:Double = 10
+let timeoutIntervalForRequest:Double = 10//默认10s 网络请求超时时间
 
-@objc protocol WRNetWrapperDelegate:NSObjectProtocol
-{
-    // TODO：研究 如果把result的类型改为Any会怎么样
+@objc protocol XLsn0wNetworkingDelegate:NSObjectProtocol {
     @objc optional func netWortDidSuccess(result:AnyObject, requestName:String, parameters:NSDictionary?);
     @objc optional func netWortDidFailed (result:AnyObject, error:Error?, requestName:String, parameters:NSDictionary?);
 }
 
-class WRNetWrapper: NSObject
-{
-    var delegate:WRNetWrapperDelegate?
-    static var sessionManager:SessionManager? = nil
-    
+class XLsn0wNetworking: NSObject {
+    var delegate:XLsn0wNetworkingDelegate?
+    static var sessionManager:SessionManager?
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+    /**********************************************************************************************/
     /// 闭包回调请求(类方法)
     ///
     /// - Parameters:
@@ -35,27 +31,23 @@ class WRNetWrapper: NSObject
     ///   - url: 可以是字符串，也可以是URL
     ///   - parameters: 参数字典
     ///   - finishedCallback: 完成请求的回调
-    class func request(method:HTTPMethod, url:String, parameters:NSDictionary?, finishedCallback:  @escaping (_ result : AnyObject, _ error: Error?) -> ())
-    {
+    class func request(method:HTTPMethod, url:String, parameters:NSDictionary?, finishedCallback:  @escaping (_ result : AnyObject, _ error: Error?) -> ()) {
         let config:URLSessionConfiguration = URLSessionConfiguration.default
-        config.timeoutIntervalForRequest = NetworkTimeoutInterval
+        config.timeoutIntervalForRequest = timeoutIntervalForRequest
         sessionManager = SessionManager(configuration: config)
         
-        Alamofire.request(url, method: method, parameters: parameters as? Parameters).responseJSON
-        { (response) in
+        Alamofire.request(url, method: method, parameters: parameters as? Parameters).responseJSON { (response) in
             let data = response.result.value
-            if (response.result.isSuccess)
-            {
+            if (response.result.isSuccess) {
                 finishedCallback(data as AnyObject, nil)
-            }
-            else
-            {
+            } else {
                 finishedCallback(data as AnyObject,response.result.error)
             }
         }
     }
-    
-    
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+    /**********************************************************************************************/
     /// 代理方法请求(类方法)
     ///
     /// - Parameters:
@@ -64,23 +56,20 @@ class WRNetWrapper: NSObject
     ///   - requestName: 请求名字，一个成功的代理方法可以处理多个请求，所以用requestName来区分具体请求
     ///   - parameters: 参数字典
     ///   - delegate: 代理对象
-    class func requestDelegate(method:HTTPMethod, url:String, requestName:String, parameters:NSDictionary?, delegate:AnyObject)
-    {
+    class func requestDelegate(method:HTTPMethod, url:String, requestName:String, parameters:NSDictionary?, delegate:AnyObject) {
         let config:URLSessionConfiguration = URLSessionConfiguration.default
-        config.timeoutIntervalForRequest = NetworkTimeoutInterval
+        config.timeoutIntervalForRequest = timeoutIntervalForRequest
         sessionManager = SessionManager(configuration: config)
-        Alamofire.request(url, method: method, parameters: parameters as? Parameters).responseJSON
-        { (response) in
+        Alamofire.request(url, method: method, parameters: parameters as? Parameters).responseJSON { (response) in
             let data = response.result.value
-            if (response.result.isSuccess)
-            {
+            if (response.result.isSuccess) {
                 delegate.netWortDidSuccess?(result: data as AnyObject, requestName: requestName, parameters: parameters)
-            }
-            else
-            {
+            } else {
                 delegate.netWortDidFailed?(result: data as AnyObject, error:response.error, requestName: requestName, parameters: parameters)
             }
         }
     }
-    
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+    /**********************************************************************************************/
 }
