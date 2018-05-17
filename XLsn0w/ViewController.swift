e5d9468a1c27ec449542ec9fc0b12364f371feb8
xlsn0w <xlsn0wios@gmail.com>
2018-05-17 17:40:30 +0800

    update

--- XLsn0w/ViewController.swift
+++ XLsn0w/ViewController.swift
@@ -13,12 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let scrollView = XLsn0wSwiftLoop(frame: CGRect(x:0, y:100, width:CGFloat(kScreenWidth), height:200))
+        let xlsn0wLoop = XLsn0wSwiftLoop(frame: CGRect(x:0, y:100, width:CGFloat(kScreenWidth), height:200))
         let array:NSArray = ["1.jpg", "2.jpg", "3.jpg"]
-        scrollView.dataArray = array
-        scrollView.cuttentPageColor = UIColor.red
-        scrollView.pageIndicatorTintColor = UIColor.blue
-        view.addSubview(scrollView)
+        xlsn0wLoop.dataArray = array
+        xlsn0wLoop.cuttentPageColor = UIColor.red
+        xlsn0wLoop.pageIndicatorTintColor = UIColor.blue
+        view.addSubview(xlsn0wLoop)
     }
     
     override func didReceiveMemoryWarning() {
